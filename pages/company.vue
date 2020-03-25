b172f0af11f26d68f2a654720aeb2e69ce74a6bc
hitoshiNi <h2shi6ra@gmail.com>
2020-03-25 21:24:15 +0900

    initial comit

---
+++ pages/company.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container">
+    <div class="header">
+    <img src="../assets/icon.png" class="icon">
+    </div>
+    <img src="../assets/main_logo.png" class="main_logo">
+    
+    <div class="contents">
+      <div class="content">
+       <section class="detail">
+         <h1>会社概要</h1>
+         <pre>社名：ヒルボックス株式会社<br>     HILLBOX inc.</pre>
+         <pre>所在地：〒153-0061<br>      東京都目黒区中目黒5-28-18-202</pre>
+         <p>代表取締役：岡友紀</p>
+         <p>問い合わせ：info@hillbox.net</p>
+       </section>
+       </div>
+
+    </div>  
+    <div class="links">
+       <router-link to="/privacy" class="link_to_privacy"> ▷Privacy policy</router-link>
+
+    </div>
+    </div>
+</template>
+
+<script>
+import Logo from "~/components/Logo.vue";
+
+
+export default {
+  components: {
+    Logo,
+  }
+};
+</script>
+
+<style>
+@import "../assets/design.css";
+</style>
+
+
---
+++ pages/index.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="container">
+    <div class="header">
+    <div class="menu">
+      <button v-on:click="open" class="button" >menu</button>
+      <div class="hidden" v-show="isopen">
+        <button v-on:click="close" class="button">×</button>
+        <p><router-link to="/company">▷HILLBOX inc.</router-link></p>
+        <p><router-link to="/company">▷会社概要</router-link></p>
+        <p><router-link to="/">▷マーケティング・PR企画</router-link></p>
+        <p><router-link to="/">▷デジタルマーケティング</router-link></p>
+        <p><router-link to="/">▷アプリ/WEBサービス開発</router-link></p>
+        <p><router-link to="/privacy" class="link_to_privacy">▷Privacy policy</router-link></p>
+      </div>
+    </div>
+    <img src="../assets/icon.png" class="icon">
+    </div>
+    <img src="../assets/main_logo.png" class="main_logo">
+    
+    <div class="contents">
+      <div class="content">
+        <img src="../assets/robotY.png" class="robo">
+       <section class="detail">
+         <h1>マーケティング・PR企画</h1>
+         <p>〇課題抽出から、コンセプト設計、商品企画、コミュニケーション提案の実行まで</p>
+         <p>〇マス広告、イベント、SNS広告、PRなど</p>
+       </section>
+      </div>
+       <div class="content">
+         <img src="../assets/robotR.png" class="robo">
+       <section class="detail">
+         <h1>デジタルマーケティング</h1>
+         <p>〇LINEなどを活用したマーケティング施策のコンサルティングから開発、実行</p>
+         <p>〇公式アカウント、LINE BOT、運用広告など</p>
+       </section>
+       </div>
+       <div class="content">
+         <img src="../assets/robotG.png" class="robo">
+       <section class="detail">
+         <h1>アプリ/WEBサービス開発</h1>
+         <p>〇アプリやWEBサービス、ITシステムの企画から開発まで</p>
+         <p>〇新規サービス、業務効率化、ECサイトなど</p>
+       </section>
+      </div>
+
+    </div>  
+    <div class="links">
+        <router-link to="/privacy" class="link_to_privacy"> ▷Privacy policy</router-link>
+        <router-link to="/company" class="link_to_company"> ▷HILLBOX inc.</router-link>
+    </div>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isopen: false,
+    }
+  },
+  methods: {
+    open: function(){
+      this.isopen = true;
+      console.log(this.isopen);
+    },
+    close: function(){
+      this.isopen = false;
+    }
+    }
+  }
+
+</script>
+
+<style>
+@import "../assets/design.css";
+</style>
+
+
